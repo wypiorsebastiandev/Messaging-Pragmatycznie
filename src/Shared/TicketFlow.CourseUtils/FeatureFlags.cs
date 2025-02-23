76ba36bd3705b650ce66c5eafab8ce96520bcc54
GooRiOn <foreverframenet@gmail.com>
2025-02-23 17:40:58 +0100

    M2L3

--- src/Shared/TicketFlow.CourseUtils/FeatureFlags.cs
+++ src/Shared/TicketFlow.CourseUtils/FeatureFlags.cs
@@ -2,7 +2,7 @@
 
 public static class FeatureFlags
 {
-    public const bool UseSynchronousIntegration = false;
+    public const bool UseSynchronousIntegration = true;
     public const bool UseTopicPerTypeExample = false;
     public const bool UseListenToYourselfExample = false;
     public const bool UsePartitioningExample = false;
