aeb16573e99b91f0549abc42247f2bf4cd8b6ac5
Michał Wilczyński <michal@wilczynski.pro>
2025-03-01 12:56:32 +0100

    M8L3

--- src/Tickets/TicketFlow.Services.Tickets.Core/Messaging/TicketsConsumerService.cs
+++ src/Tickets/TicketFlow.Services.Tickets.Core/Messaging/TicketsConsumerService.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TicketFlow.CourseUtils;
 using TicketFlow.Services.Tickets.Core.Messaging.Consuming.DeadlinesCalculated;
@@ -9,13 +10,30 @@ using TicketFlow.Shared.Messaging;
 
 namespace TicketFlow.Services.Tickets.Core.Messaging;
 
-internal sealed class TicketsConsumerService(IMessageConsumer messageConsumer, AnomalySynchronizationConfigurator anomalyConfigurator) : BackgroundService
+internal sealed class TicketsConsumerService(
+    IMessageConsumer messageConsumer, 
+    AnomalySynchronizationConfigurator anomalyConfigurator, 
+    IServiceProvider serviceProvider) : BackgroundService
 {
     public const string SLAChangesQueue = "tickets-sla-changes";
     public const string TicketCreatedQueue = "tickets-ticket-created";
     
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        await messageConsumer.ConsumeMessage<InquirySubmitted>(queue: "inquiry-submitted-tickets-service-queue");
+        // await messageConsumer.ConsumeMessage<InquirySubmitted>(queue: "inquiry-submitted-tickets-service-queue");
+
+        while (stoppingToken.IsCancellationRequested is false)
+        {
+            await messageConsumer.GetMessage<InquirySubmitted>(async message =>
+                {
+                    var iocScope = serviceProvider.CreateScope();
+                    var handler = iocScope.ServiceProvider.GetService<IMessageHandler<InquirySubmitted>>();
+                    await handler!.HandleAsync(message, stoppingToken);
+                },
+                queue: "inquiry-submitted-tickets-service-queue",
+                cancellationToken: stoppingToken);
+            
+            await Task.Delay(100, stoppingToken);
+        }
     }
 }
